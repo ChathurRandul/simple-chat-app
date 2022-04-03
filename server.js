f3e60fdb1cc905701b805b92c390ebf745371271
ChathurRandul <chathur.randul@gmail.com>
2022-04-03 17:39:52 +0530

    Create get messages service

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const app = express();
 
 app.use(express.static(__dirname));
+
+let messages = [
+    { name: 'Tim', message: 'Hi' },
+    { name: 'Jane', message: 'Hello' }
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
 const server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port);
 });
