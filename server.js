b9ab07b2d1718ff5c6aa14c7d1c8900e78d44381
ChathurRandul <chathur.randul@gmail.com>
2022-04-05 11:27:24 +0530

    Remove messages using nested callbacks

--- server.js
+++ server.js
@@ -29,6 +29,15 @@ app.post('/messages', (req, res) => {
     message.save((err) => {
         if (err) sendStatus(500);
 
+        Message.findOne({ message: 'badword' }, (err, censored) => {
+            if (censored) {
+                console.log('censored words found', censored);
+                Message.remove({ _id: censored._id }, (err) => {
+                    console.log('remove censored message');
+                });
+            }
+        });
+
         io.emit('message', req.body);
         res.sendStatus(200);
     });
